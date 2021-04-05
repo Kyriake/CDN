18a792a7eb00f487511b25ad7327d52abaf628c8
LynX Gian <v.davide101@gmail.com>
2021-04-05 09:25:24 -0400

    🎉 Project Files

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/gofiber/fiber
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.7.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"time"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+)
+func main() {
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("refresh") == "true"
+		},
+		Expiration: 30 * time.Second,
+		CacheControl: true,
+	
+	}))
+	app.Use(favicon.New(favicon.Config{
+		File: "./images/favicon.ico",
+	}))
+
+	app.Static("/images", "./images")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		 return c.SendFile("./home.html")
+	})
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(404).SendFile("./404.html")
+	})
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(401).SendFile("./401.html")
+	})
+	log.Fatal(app.Listen("0.0.0.0:3000"))
+}
+
+
