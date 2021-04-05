9dcd72f9fd4ac0528df2bc9214cabaa6809ff8bf
LynX Gian <v.davide101@gmail.com>
2021-04-05 09:38:05 -0400

    💡 Commented Things Out

--- main.go
+++ main.go
@@ -21,20 +21,25 @@ func main() {
 		CacheControl: true,
 	
 	}))
+	//website favicon
 	app.Use(favicon.New(favicon.Config{
 		File: "./images/favicon.ico",
 	}))
 
+
 	app.Static("/images", "./images")
 
+	//returns the html page when you first load up the site	
 	app.Get("/", func(c *fiber.Ctx) error {
 		 return c.SendFile("./site/home.html")
 	})
 
+	//returns an html file when their is a 404 error
 	app.Use(func(c *fiber.Ctx) error {
 		return c.Status(404).SendFile("./site/404.html")
 	})
 
+	//returns an html file when their is a 401 error
 	app.Use(func(c *fiber.Ctx) error {
 		return c.Status(401).SendFile("./site/401.html")
 	})
